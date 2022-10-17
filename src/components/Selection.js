c6d21220d2a9c382e046dd312564bbfe727b4fee
krishna <krishna.rai@informa.com>
2022-10-17 22:08:14 +0530

    completed the assignment

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,14 @@
+import React, { useState } from "react"
+import '../styles/Child.css'
 
+
+const Selection = ({applyColor}) => {
+    const [selectionStyle , updateSelectionStyle] = useState({})
+    return (
+        <div className="fix-box" style={selectionStyle} onClick={()=>{applyColor(updateSelectionStyle)}}>
+            <h2 className="subheading">Selection</h2>
+        </div>
+    )
+}
+
+export default Selection;
